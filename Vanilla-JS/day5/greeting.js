96d28b900ae84c22982bbe6f9d4e3ea95b3013ed
powerticket <jwp0530@gmail.com>
2020-10-11 00:02:09 +0900

    edit greeting.js

--- Vanilla-JS/day5/greeting.js
+++ Vanilla-JS/day5/greeting.js
@@ -7,12 +7,26 @@ const USER_LS = "currentUser";
 const SHOWING = "showing";
 
 function printGreeting(text) {
+  form.classList.remove(SHOWING);
   greeting.classList.add(SHOWING);
   greeting.innerText = `Hello ${text}`;
 }
 
+function handleSubmit(event) {
+  event.preventDefault();
+  const currentValue = input.value;
+  saveName(currentValue);
+  printGreeting(currentValue);
+  
+}
+
+function saveName(text) {
+  localStorage.setItem(USER_LS, text);
+}
+
 function askName() {
   form.classList.add(SHOWING);
+  form.addEventListener("submit", handleSubmit)
 }
 
 function loadName() {
